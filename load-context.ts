c932e77416396aaa8d39742f1c0cc8e260b8b124
Katherine Ebel <hello@katherineebel.com>
2024-11-13 07:28:06 -0500

    installed drizzle and setup with d1 database

--- load-context.ts
+++ load-context.ts
@@ -1,9 +1,39 @@
-import { type PlatformProxy } from "wrangler";
+import { AppLoadContext } from '@remix-run/cloudflare'
+import { drizzle } from 'drizzle-orm/d1'
+import { type PlatformProxy } from 'wrangler'
 
-type Cloudflare = Omit<PlatformProxy<Env>, "dispose">;
+export const connection = (d1: D1Database) => {
+  return drizzle(d1)
+}
 
-declare module "@remix-run/cloudflare" {
+export type Cloudflare = Omit<PlatformProxy<Env>, 'dispose' | 'cf'>
+
+declare module '@remix-run/cloudflare' {
   interface AppLoadContext {
-    cloudflare: Cloudflare;
+    cloudflare: Cloudflare
+    db: () => ReturnType<typeof connection>
+  }
+}
+
+interface CloudflareLoadContext {
+  context: {
+    cloudflare: Cloudflare
+  }
+  request: Request
+}
+
+let dbConnection: ReturnType<typeof connection> | null = null
+
+export function getLoadContext({
+  context,
+}: CloudflareLoadContext): AppLoadContext {
+  return {
+    cloudflare: context.cloudflare,
+    db() {
+      if (!dbConnection) {
+        dbConnection = connection(context.cloudflare.env.DB)
+      }
+      return dbConnection
+    },
   }
 }
