0cc3be23a1454934a28448dcaaa625244788d8ad
Katherine Ebel <hello@katherineebel.com>
2024-11-13 12:21:37 -0500

    Add migration support and schema for database
    Introduced migrations directory and configuration in `wrangler.toml`. Added a new schema for database tables in `schema.server.ts`. Updated `package.json` scripts to include migration commands.

--- load-context.ts
+++ load-context.ts
@@ -3,7 +3,7 @@ import { drizzle } from 'drizzle-orm/d1'
 import { type PlatformProxy } from 'wrangler'
 
 export const connection = (d1: D1Database) => {
-  return drizzle(d1)
+  return drizzle(d1, { casing: 'snake_case' })
 }
 
 export type Cloudflare = Omit<PlatformProxy<Env>, 'dispose' | 'cf'>
